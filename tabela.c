67760a4f7daeb7e15d87519f4424bb692f102b84
gabhanisch <gabhafonso@gmail.com>
2026-06-12 15:17:25 -0300

    Segundo commit

--- tabela.c
+++ tabela.c
@@ -51,3 +51,52 @@ int palavraExiste(Hash *h, char *palavra) {
     return 0;
 }
 
+void inserir(Hash *h, char *palavra, char *definicao) {
+
+    if (palavraExiste(h, palavra)) {
+        printf("\nEssa palavra ja existe no dicionario!\n");
+        return;
+    }
+
+    int indice = funcaoHash(palavra);
+
+    if (h->tabela[indice] != NULL) {
+        h->colisoes++;
+    }
+
+    No *novo = (No *) malloc(sizeof(No));
+
+    if (novo == NULL) {
+        printf("Erro de alocacao de memoria!\n");
+        return;
+    }
+
+    strcpy(novo->palavra, palavra);
+    strcpy(novo->definicao, definicao);
+
+    novo->prox = h->tabela[indice];
+    h->tabela[indice] = novo;
+
+    h->elementos++;
+
+    printf("\nPalavra inserida com sucesso!\n");
+}
+
+void buscar(Hash *h, char *palavra) {
+
+    int indice = funcaoHash(palavra);
+
+    No *aux = h->tabela[indice];
+
+    while (aux != NULL) {
+
+        if (strcmp(aux->palavra, palavra) == 0) {
+
+            printf("\n================================");
+            printf("\nPalavra: %s", aux->palavra);
+            printf("\nDefinicao: %s", aux->definicao);
+            printf("\n================================\n");
+
+            return;
+        }
+
